d7d9cbc94668178c5a56934738d0b1b44749e284
alona <aloyna.vasilieva@gmail.com>
2023-09-24 22:27:28 +0200

    add project arena hw

---
+++ scripts/script.js
@@ -0,0 +1,36 @@
+// const root = document.querySelector('#root')
+
+// //секция 1
+// const aboutUs = document.createElement('section')
+// aboutUs.classList.add('about-us')
+// root.appendChild(aboutUs)
+
+// const headersSection = document.createElement('h1')
+// const textInHeader = document.createTextNode('РЕАЛИЗУЕМ КРУПНЕЙШИЕ ПРОЕКТЫ В МИРЕ')
+// headersSection.appendChild(textInHeader)
+
+// const paragraphSection = document.createElement('p')
+// const textInParagraph = document.createTextNode(стадионы, газопроводы, мосты, дамбы)
+// paragraphSection.appendChild(textInParagraph)
+
+// const divForItems = document.createElement('div')
+// divForItems.classList.add('div-for-items')
+
+// for (let i = 0; i <= 3; i++) {
+//     const numberItem = document.createElement('div');
+//     numberItem.classList.add('number-item')
+//     const firstNumberItem = document.createElement('h3')
+//     firstNumberItem.textContent = '26'
+//     const secondNumberItem = document.createElement('h4')
+//     secondNumberItem.textContent = 'лет'
+//     const thirdNumberItem = document.createElement('h5')
+//     thirdNumberItem.textContent = 'на рынке'
+//     numberItem.appendChild(firstNumberItem)
+//     numberItem.appendChild(secondNumberItem)
+//     numberItem.appendChild(thirdNumberItem)
+//     divForItems.appendChild(numberItem)    
+// }
+
+// aboutUs.appendChild(headersSection)
+// aboutUs.appendChild(paragraphSection)
+// aboutUs.appendChild(divForItems)
